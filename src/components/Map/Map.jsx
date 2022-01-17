461190facc9b512617308a9c11ebd6588e831ff6
Rafael Estrada <rafaelestrada@comcast.net>
2022-01-16 21:51:08 -0800

    added google map api

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import {Paper,Typography,useMediaQuery} from '@material-ui/core';
-import LocationOutLinedIcon from '@material-ui/icons/LocationOnOutlined';
+import LocationOnOutLinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab';
 import useStyles from './styles';
 
